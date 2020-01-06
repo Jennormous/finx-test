67986ceccea909a70ee3593c41c90247b210a00c
jennormous <smj.boudreau@gmail.com>
2020-01-05 21:39:43 -0800

    Updates

---
+++ precache-manifest.259676ea68079a8e2c1ad4e60d559926.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a3f21d4b4eb5a4ac6ed4d7872d3730",
+    "url": "/finx-test/index.html"
+  },
+  {
+    "revision": "e676c3b8d5cc1123d556",
+    "url": "/finx-test/static/css/main.e86dc0d7.chunk.css"
+  },
+  {
+    "revision": "bfda92ef98811cab3c76",
+    "url": "/finx-test/static/js/2.f4f74cef.chunk.js"
+  },
+  {
+    "revision": "f84652c970d19859bdbfb2dca71c8cd4",
+    "url": "/finx-test/static/js/2.f4f74cef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e676c3b8d5cc1123d556",
+    "url": "/finx-test/static/js/main.29f8b65c.chunk.js"
+  },
+  {
+    "revision": "a43959845ff8170c8f0a",
+    "url": "/finx-test/static/js/runtime-main.fd2ed814.js"
+  },
+  {
+    "revision": "a1557a265acaa6756416559b22d989cb",
+    "url": "/finx-test/static/media/calendar-alt-regular.a1557a26.svg"
+  },
+  {
+    "revision": "06cba8e0f907e6c74642ae2e8282fe4f",
+    "url": "/finx-test/static/media/finex-logo.06cba8e0.png"
+  },
+  {
+    "revision": "8ea3c0bc2e6a0df3df2c2fdc856b8ccd",
+    "url": "/finx-test/static/media/hondalogo.8ea3c0bc.png"
+  }
+]);
